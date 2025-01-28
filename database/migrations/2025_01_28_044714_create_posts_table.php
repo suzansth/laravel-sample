097b63933dd715a4db23681646aa5091199e4ac4
suzansth <pujanstha89@gmail.com>
2025-01-28 14:18:58 +0900

    Update 2025_01_28_044714_create_posts_table.php

--- database/migrations/2025_01_28_044714_create_posts_table.php
+++ database/migrations/2025_01_28_044714_create_posts_table.php
@@ -4,24 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePostsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->bigIncrements('id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('name', 255);
+            $table->text('contents');
+            $table->timestamps(); // 作成日時、更新日時のカラムを追加
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('posts');
     }
-};
+}
