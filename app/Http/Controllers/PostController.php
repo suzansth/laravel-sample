b6fcf42045f7e3bbddfe5575ad26cd861285aa73
taro.tanaka <taro.tanaka@example.com>
2025-01-31 15:12:12 +0900

    課題13

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
+
+use App\Http\Controllers;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
     //
 }
